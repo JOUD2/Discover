9a3ec643342f41b2e0cc6f13d87aebde0eefa435
Joud <jjoud614@gmail.com>
2023-11-21 11:17:34 +0300

    last

---
+++ Discover.js
@@ -0,0 +1,92 @@
+//Advertisement
+document.addEventListener("DOMContentLoaded", function() {
+  var advertisement = document.getElementById("advertisement");
+
+  // Check if advertisement should be displayed
+  var isAdvertisementDisplayed = localStorage.getItem("advertisementDisplayed");
+
+  if (!isAdvertisementDisplayed) {
+    setTimeout(function() {
+      advertisement.style.display = "flex";
+    }, 5000);
+  }
+
+  var closeButton = document.getElementById("close-button");
+
+  closeButton.addEventListener("click", function() {
+    advertisement.style.display = "none";
+
+    // Set flag indicating that the advertisement has been viewed
+    localStorage.setItem("advertisementDisplayed", true);
+  });
+});
+
+// Modal Image Gallery
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+  var captionText = document.getElementById("caption");
+  captionText.innerHTML = element.alt;
+}
+
+// Toggle between showing and hiding the sidebar when clicking the menu icon
+var mySidebar = document.getElementById("mySidebar");
+
+function w3_open() {
+  if (mySidebar.style.display === 'block') {
+    mySidebar.style.display = 'none';
+  } else {
+    mySidebar.style.display = 'block';
+  }
+}
+
+// Close the sidebar with the close button
+function w3_close() {
+    mySidebar.style.display = "none";
+}
+
+//ShowSlides
+window.onload= function () {
+ setInterval(function(){ 
+     plusSlides(1);
+ }, 3000);
+ }
+    var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+window.onload= function () {
+ setInterval(function(){ 
+     plusSlides(1);
+ }, 3000);
+ }
+
+//Snackbar
+ function myFunction() {
+  var x = document.getElementById("snackbar");
+  x.className = "show";
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
+
